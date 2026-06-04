6c5eab59981a80889770c83d8b22683099899255
Devoloper12 <94070423+Devoloper12@users.noreply.github.com>
2026-06-04 23:48:47 +0530

    Change window load event to use on() method

--- effect.js
+++ effect.js
@@ -1,7 +1,8 @@
-$(window).load(function(){
-	$('.loading').fadeOut('fast');
-	$('.container').fadeIn('fast');
+$(window).on('load', function(){
+    $('.loading').fadeOut('fast');
+    $('.container').fadeIn('fast');
 });
+
 $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
